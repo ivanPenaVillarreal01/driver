1063384779bd6c4a81bf44c5ed2d15eaa4e07e96
ivanPenaVillarreal01 <ivan.penavillarreal01@utrgv.edu>
2025-09-21 09:44:22 -0500

    Add files via upload

---
+++ queueLL.h
@@ -0,0 +1,29 @@
+
+class queueLL
+{
+private:
+	//put what you need here...
+
+public:
+	queueLL()
+	{}
+
+	~queueLL()
+	{}
+
+	//add item to back of queue
+	void enqueue(int x)
+	{}
+
+	//remove and return first item from queue
+	int dequeue()
+	{}
+
+	//return true if empty, false otherwise.
+	bool empty()
+	{}
+
+	//For the final part of the test program, template this class
+	//and add a decimate method.
+
+}
